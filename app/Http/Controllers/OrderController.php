e7c0bad782be2e65c276b6fd08f8b790b302858b
User <user@example.com>
2026-05-10 21:31:14 +0700

    add filter

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -24,6 +24,14 @@ class OrderController extends Controller
             $query->where('status', $request->status);
         }
 
+        if ($request->start_date) {
+            $query->whereDate('created_at', '>=', $request->start_date);
+        }
+
+        if ($request->end_date) {
+            $query->whereDate('created_at', '<=', $request->end_date);
+        }
+
         return response()->json($query->get());
     }
 
